80b3f05ba51c08de71ce9f1bf7958a90ed7fb03b
[math-borges] <[m.borges@brun-invest.net]>
2023-06-07 09:20:10 +0200

    Fixed Json Response

--- src/Controller/ApiCustomerController.php
+++ src/Controller/ApiCustomerController.php
@@ -6,13 +6,18 @@ use App\Entity\Customer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
 
 class ApiCustomerController extends AbstractController
 {
     #[Route('/api/customer/{id}', name: 'app_api_customer')]
-    public function index(Customer $customer): Response
+    public function index(int $id): Response
     {
-        return $this->json( $customer,
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'http://localhost:8001/testmicroservice/' . $id);
+        $response = $response->toArray();
+        
+        return $this->json( $response,
         200,
         []);
     }
