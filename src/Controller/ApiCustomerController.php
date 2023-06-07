90314fbf4232970c1026f4d6f9f388addf8a380d
NicolasSanna <nico13sanna@gmail.com>
2023-06-07 11:45:57 +0200

    CRUD terminé

--- src/Controller/ApiCustomerController.php
+++ src/Controller/ApiCustomerController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\HttpFoundation\Request;
 
 class ApiCustomerController extends AbstractController
 {
@@ -21,6 +22,28 @@ class ApiCustomerController extends AbstractController
         return $this->json($customers, 200);
     }   
 
+    #[Route('/api/customers/customer/add', name: 'app_api_add_customer', methods: ['POST'])]
+    public function createCustomer(Request $request): Response
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $objectToArray = [
+            'firstname' => $data['firstname'],
+            'lastname' => $data['lastname']
+        ];
+
+        $client = HttpClient::create();
+        $response = $client->request('POST', 'http://localhost:8001/testmicroservice/customers/customer/create', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'body' => json_encode($objectToArray), // Remplacez les données avec les valeurs appropriées
+        ]);
+        $data = $response->toArray();
+        
+        return $this->json($data, 200);
+    }
+
     #[Route('/api/customers/customer/{id}', name: 'app_api_customer')]
     public function getCustomer(int $id): Response
     {
@@ -32,44 +55,36 @@ class ApiCustomerController extends AbstractController
         return $this->json($customer, 200);
     }    
     
-    #[Route('/api/customers/customer/add', name: 'app_api_add_customer', methods: ['POST'])]
-    public function createCustomer(): Response
-    {
-        $customer = new Customer();
+    
 
-        $client = HttpClient::create();
-        $response = $client->request('POST', 'http://localhost:8001/testmicroservice/', [
-            'headers' => [
-                'Content-Type' => 'application/json',
-            ],
-            'body' => $customer, // Remplacez les données avec les valeurs appropriées
-        ]);
-        $data = $response->getContent();
-        
-        return $this->json($data, 200);
-    }
-
-    #[Route('/api/customers/customer/{id}', name: 'app_api_del_customer', methods: ['DELETE'])]
+    #[Route('/api/customers/customer/delete/{id}', name: 'app_api_del_customer', methods: ['DELETE'])]
     public function deleteCustomer(int $id): Response
     {
         $client = HttpClient::create();
-        $response = $client->request('DELETE', 'http://localhost:8001/testmicroservice/' . $id);
+        $response = $client->request('DELETE', 'http://localhost:8001/testmicroservice/customers/customer/delete/' . $id);
         $data = $response->getContent();
         
-        return $this->json($data, 200);
+        return $this->json(json_decode($data), 200);
     }
 
     #[Route('/api/customers/customer/edit/{id}', name: 'app_api_edit_customer', methods: ['PUT'])]
-    public function editCustomer(int $id): Response
+    public function editCustomer(int $id, Request $request): Response
     {
+        $data = json_decode($request->getContent(), true);
+
+        $objectToArray = [
+            'firstname' => $data['firstname'],
+            'lastname' => $data['lastname']
+        ];
+        
         $client = HttpClient::create();
-        $response = $client->request('PUT', 'http://localhost:8001/testmicroservice/' . $id, [
+        $response = $client->request('PUT', 'http://localhost:8001/testmicroservice/customers/customer/edit/' . $id, [
             'headers' => [
                 'Content-Type' => 'application/json',
             ],
-            'body' => '{"name": "Updated Name"}', // Remplacez les données avec les valeurs appropriées
+            'body' => json_encode($objectToArray), // Remplacez les données avec les valeurs appropriées
         ]);
-        $data = $response->getContent();
+        $data = json_decode($response->getContent(), true);
         
         return $this->json($data, 200);
     }
