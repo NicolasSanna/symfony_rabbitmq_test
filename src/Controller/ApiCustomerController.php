c952cf22a05c72ef6352a44ac55fb24a5b7e6294
[math-borges] <[m.borges@brun-invest.net]>
2023-06-07 09:58:22 +0200

    Edited customer api

--- src/Controller/ApiCustomerController.php
+++ src/Controller/ApiCustomerController.php
@@ -10,15 +10,65 @@ use Symfony\Component\HttpClient\HttpClient;
 
 class ApiCustomerController extends AbstractController
 {
-    #[Route('/api/customer/{id}', name: 'app_api_customer')]
-    public function index(int $id): Response
+    #[Route('/api/customers', name: 'app_api_customers')]
+    public function getCustomers(): Response
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'http://localhost:8001/testmicroservice/');
+        $data = $response->getContent();
+        
+        return $this->json($data, 200);
+    }   
+
+    #[Route('/api/customers/customer/{id}', name: 'app_api_customer')]
+    public function getCustomer(int $id): Response
     {
         $client = HttpClient::create();
         $response = $client->request('GET', 'http://localhost:8001/testmicroservice/' . $id);
-        $response = $response->toArray();
+        $data = $response->getContent();
         
-        return $this->json( $response,
-        200,
-        []);
+        return $this->json($data, 200);
+    }    
+    
+    #[Route('/api/customers/customer/add', name: 'app_api_add_customer', methods: ['POST'])]
+    public function createCustomer(): Response
+    {
+        $customer = new Customer();
+
+        $client = HttpClient::create();
+        $response = $client->request('POST', 'http://localhost:8001/testmicroservice/', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'body' => $customer, // Remplacez les données avec les valeurs appropriées
+        ]);
+        $data = $response->getContent();
+        
+        return $this->json($data, 200);
+    }
+
+    #[Route('/api/customers/customer/{id}', name: 'app_api_del_customer', methods: ['DELETE'])]
+    public function deleteCustomer(int $id): Response
+    {
+        $client = HttpClient::create();
+        $response = $client->request('DELETE', 'http://localhost:8001/testmicroservice/' . $id);
+        $data = $response->getContent();
+        
+        return $this->json($data, 200);
+    }
+
+    #[Route('/api/customers/customer/edit/{id}', name: 'app_api_edit_customer', methods: ['PUT'])]
+    public function editCustomer(int $id): Response
+    {
+        $client = HttpClient::create();
+        $response = $client->request('PUT', 'http://localhost:8001/testmicroservice/' . $id, [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'body' => '{"name": "Updated Name"}', // Remplacez les données avec les valeurs appropriées
+        ]);
+        $data = $response->getContent();
+        
+        return $this->json($data, 200);
     }
 }
